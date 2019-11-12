e44e43cd6980d0a86916734e865bb0a5a5fd6e88
maryfarner18 <maryfarner@gmail.com>
2019-11-12 14:19:04 -0500

    another days work

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const FROG_URL = "/frogs"
     const VEHICLE_URL = "/vehicles"
     const LANE_URL = "/lanes"
-    const CAR_MOVE_TIME = 100 // in ms
-    const CAR_ADD_TIME = 650 // in ms
+    const CAR_MOVE_TIME = 200 // in ms
+    const CAR_ADD_TIME = 1000 // in ms
     const FROG_MOVE_INC = 10; // in px
     const VEHICLE_MOVE_INC = 10; // in px
 
@@ -13,22 +13,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let gameHolder = document.getElementById("game-holder")
     let frog
     let frogTag = document.getElementById("frog")
+    let countDownTag = document.getElementById("three-count")
+    let pauseTag = document.getElementById("pause-display")
+    let deathDiv = document.getElementById("death-overlay")
+    let lifeDiv = document.getElementById("lives-overlay")
+    let startTag = document.getElementById("start-overlay")
+    let prizeTag = document.getElementById("prize")
+    let winTag = document.getElementById("win-overlay")
+
 
     //Data storage
     let lanes = []
-    
-    let vehicles = []
+    let vehicles = [] // array of html elements
+    let lives = 3
 
     // flags
     let paused = true;
     let firstGo = true;
     let level = 1;
+    let count = 3;
+    let locked = false
 
     //intervals
     let moveCarInterval
     let addCarInterval 
     let freezeInterval
-    
+    let countDownInterval
 
     //game dimensions
     const WIDTH = gameHolder.offsetWidth
@@ -46,6 +56,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if (left > LEFT_EDGE + 5) {
             frogTag.style.left = `${left - FROG_MOVE_INC}px`;
         }
+        checkPrize()
+        checkVehicles()
     }
 
 
@@ -56,6 +68,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if (left < RIGHT_EDGE - 35) {
         frogTag.style.left = `${left + FROG_MOVE_INC}px`;
         }
+        checkPrize()
+        checkVehicles()
     }
 
     function movefrogUp() {
@@ -64,6 +78,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if (top > TOP_EDGE + 7) {
         frogTag.style.top = `${top - FROG_MOVE_INC}px`;
         }
+        checkPrize()
+        checkVehicles()
     }
     
     function movefrogDown() {
@@ -73,22 +89,143 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if (top < BOTTOM_EDGE - 35) {
             frogTag.style.top = `${top + FROG_MOVE_INC}px`;
         }
+        checkPrize()
+        checkVehicles()
+    }
+    function checkPrize(){
+        let buffer = 7
+        frogXMin = parseInt(frogTag.style.left.replace("px", "")) + buffer
+        frogXMax = frogXMin + frogTag.offsetWidth - 2*buffer
+        frogYMax = parseInt(frogTag.style.top.replace("px", "")) - buffer
+        frogYMin = frogYMax - frogTag.offsetHeight + 2*buffer
+
+        prizeXMin = parseInt(prizeTag.style.left.replace("px", "")) + buffer
+        prizeXMax = prizeXMin + prizeTag.offsetWidth - 2*buffer
+        prizeYMax = parseInt(prizeTag.style.top.replace("px", "")) - buffer
+        prizeYMin = prizeYMax - prizeTag.offsetHeight + 2*buffer
+
+        if(prizeXMin > frogXMin && prizeXMin < frogXMax || prizeXMax > frogXMin && prizeXMax < frogXMax){
+            if(prizeYMin > frogYMin && prizeYMin < frogYMax || prizeYMax > frogYMin && prizeYMax < frogYMax){
+                // THEY OVERLAP!
+                
+                youWin()
+                 
+            }
+        }
+
+    }
+
+    function checkVehicles(){
+
+        frogXMin = parseInt(frogTag.style.left.replace("px", "")) 
+        frogXMax = frogXMin + frogTag.offsetWidth
+        frogYMax = parseInt(frogTag.style.top.replace("px", ""))
+        frogYMin = frogYMax - frogTag.offsetHeight
+
+        vehicles.forEach(vehicle => {
+
+            console.log("checking a vehicle")
+
+            vehicleXMin = parseInt(vehicle.style.left.replace("px", ""))
+            vehicleXMax = vehicleXMin + vehicle.offsetWidth
+            vehicleYMax = parseInt(vehicle.style.top.replace("px", ""))
+            vehicleYMin = vehicleYMax - vehicle.offsetHeight
+            
+            // CHECK X 
+            if(vehicleXMin > frogXMin && vehicleXMin < frogXMax || vehicleXMax > frogXMin && vehicleXMax < frogXMax){
+                if(vehicleYMin > frogYMin && vehicleYMin < frogYMax || vehicleYMax > frogYMin && vehicleYMax < frogYMax){
+                    // THEY OVERLAP!
+                    
+                    frogHit()
+                    
+                    
+                }
+            }
+        })
+    }
+
+    function youWin(){
+        clearInterval(moveCarInterval)
+        clearInterval(addCarInterval)
+        document.removeEventListener("keydown", keyDownHandler)
+        winTag.style.display = "block"
+
+    }
+
+    function resetGame(){
+        location.reload()
+    }
+
+    function resetDOM(){
+        paused = true;
+        firstGo= true;
+        locked = true;
+
+        clearInterval(moveCarInterval)
+        clearInterval(addCarInterval)
+
+        lives -= 1
+
+        frogTag.style.top = `${BOTTOM_EDGE - 27}px`
+        frogTag.style.left = `${RIGHT_EDGE/2}px`
+        
+        removeVehicles()
+        vehicles = []
+
+        getStartingCars()
+
+    }
+
+    function frogHit(){
+
+        if(lives <= 1){
+            deathDiv.style.display="block"
+            clearInterval(moveCarInterval)
+            clearInterval(addCarInterval)
+            document.removeEventListener("keydown", keyDownHandler)
+            reloadInterval = setInterval(resetGame, 4000)
+
+        }else{
+        
+            let span = lifeDiv.getElementsByTagName("span")[0]
+            span.innerText = `Lives remaining: ${lives - 1}`
+            lifeDiv.style.display="block"
+  
+            resetDOM()
+        }
+    }
+
+    function checkThisVehicle(vehicle){
+        
+        frogXMin = parseInt(frogTag.style.left.replace("px", "")) 
+        frogXMax = frogXMin + frogTag.offsetWidth
+        frogYMax = parseInt(frogTag.style.top.replace("px", ""))
+        frogYMin = frogYMax - frogTag.offsetHeight
+
+        vehicleXMin = parseInt(vehicle.style.left.replace("px", ""))
+        vehicleXMax = vehicleXMin + vehicle.offsetWidth
+        vehicleYMax = parseInt(vehicle.style.top.replace("px", ""))
+        vehicleYMin = vehicleYMax - vehicle.offsetHeight
+        
+        // CHECK X 
+        if(vehicleXMin > frogXMin && vehicleXMin < frogXMax || vehicleXMax > frogXMin && vehicleXMax < frogXMax){
+            if(vehicleYMin > frogYMin && vehicleYMin < frogYMax || vehicleYMax > frogYMin && vehicleYMax < frogYMax){
+            
+                frogHit()
+            }
+        }
+
     }
     
 
     // ==================  CAR STUFF ========================================
-    
-    function addListener(){
-        console.log("adding Event Listener")
-        document.addEventListener("keydown", keyDownHandler)
-        clearInterval(freezeInterval)
-    }
 
-    // HAPPEN EVERY ~3 SECONDS
+
+    // appends a vehicle to the dom in the given lane
     function appendCar(vehicle, lane){
-        vehicles.push(vehicle)
 
-        //place on DOM based on this Lane
+        // add to the vehicle storage array
+        vehicles.push(vehicle)
 
         let y_coord = BOTTOM_EDGE - lane.height*40 - 20
         let x_coord
@@ -106,6 +243,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     }
 
+    function removeVehicles(){
+        vehicles.forEach(vehicle => {
+            gameHolder.removeChild(vehicle)
+        })
+    }
+
+    // Fetches a random car from the api to add to the given or a random lane
     function getCar(lane = lanes[Math.floor(Math.random()*5)]){
         
         fetch(`${BASE_URL}${VEHICLE_URL}?limit=1`)
@@ -119,15 +263,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 vehicle.src = data.avatar
                 vehicle.dataset.dir = lane.direction
                 appendCar(vehicle, lane)
-
-                //add keyDown listener
-                document.addEventListener("keydown", keyDownHandler) 
                 
             })
 
     }
 
-    // HAPPEN EVERY 
+    // Moves all the cars by one increment. Set with an interval
     function moveCars(){
         vehicles.forEach((vehicle, index) => {
             let left = parseInt(vehicle.style.left.replace("px", ""))
@@ -151,9 +292,106 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     // remove it from array
                 }
             }
+            checkThisVehicle(vehicle)
         })
         
     }
+
+    // HANDLER STUFF =============================================================
+
+    // adds the keydown listener to the document
+    function addListener(){
+        console.log("adding Event Listener")
+        document.addEventListener("keydown", keyDownHandler)
+        clearInterval(freezeInterval)
+    }
+
+    function changeCount(){
+        count--
+        if(count === 0 ){
+            countDownTag.innerText = "GO!"
+        }else if(count === -1){
+            countDownTag.style.display = "none"
+            clearInterval(countDownInterval)
+        }else{
+            countDownTag.innerText = count
+        }
+    }
+
+    // handles all key presses (arrows and space bar)
+    function keyDownHandler(e) {
+
+        if(!paused){
+            if (e.key === "ArrowLeft") {
+                movefrogLeft();
+            }
+            if (e.key === "ArrowRight") {
+                movefrogRight();
+            }
+            if (e.key === "ArrowUp") {
+                movefrogUp();
+            }
+            if (e.key === "ArrowDown") {
+                movefrogDown();
+            }
+            if (e.keyCode === 32){
+                console.log("PAUSING")
+                paused = !paused
+
+                //remove the car mover listeners
+                clearInterval(moveCarInterval)
+                clearInterval(addCarInterval)
+                pauseTag.style.display = "block"
+              
+                
+            }
+        }else{ // START GAME
+            
+            if(e.keyCode === 13 ){
+                lifeDiv.style.display = "none"
+                startTag.style.display = "block"
+                locked = false
+            }
+
+            //hit spacebar
+            if (e.keyCode === 32 && !locked){
+    
+                paused = !paused
+                pauseTag.style.display = "none"
+                startTag.style.display = "none"
+                
+                //Set the car move interval
+                moveCarInterval = setInterval(moveCars, CAR_MOVE_TIME)
+                
+                //Set the add car interval
+                addCarInterval = setInterval(getCar, CAR_ADD_TIME)
+                
+                if(firstGo){
+                    console.log("FIRST GO")
+                    firstGo = false
+                    count = 3;
+
+                    document.removeEventListener("keydown", keyDownHandler)
+                    console.log("removed listener") 
+                    
+                      //make the three visivle
+                    countDownTag.innerText =  count;
+                    countDownTag.style.display = "block";
+                  
+                    //set an interval for 1 second to change innerHTML
+                    countDownInterval = setInterval(changeCount, 1000)
+                    //set interval to start the listener after the 3 seconds
+                    freezeInterval = setInterval(addListener, 4000)
+                    
+                }
+                
+            }
+
+        }
+        
+    }
+
+    // Initial fetches ====================================================
 
     function getStartingCars(){
         lanes.forEach(lane => {
@@ -174,61 +412,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             })
 
     }
-    function keyDownHandler(e) {
-        console.log("handling click!")
-        if(!paused){
-            if (e.key === "ArrowLeft") {
-                movefrogLeft();
-            }
-            if (e.key === "ArrowRight") {
-                movefrogRight();
-            }
-            if (e.key === "ArrowUp") {
-                movefrogUp();
-            }
-            if (e.key === "ArrowDown") {
-                movefrogDown();
-            }
-            if (e.keyCode === 32){
-                console.log("PAUSING")
-                paused = !paused
-                //remove the car mover listeners
-                clearInterval(moveCarInterval)
-                clearInterval(addCarInterval)
-                
-                //pause game timer
-                
-            }
-        }else{ // START GAME
-            
-            //hit spacebar
-            if (e.keyCode === 32){
-                
-           
-                paused = !paused
-                
-                //Set the car move interval
-                moveCarInterval = setInterval(moveCars, CAR_MOVE_TIME)
-                
-                //Set the add car interval
-                addCarInterval = setInterval(getCar, CAR_ADD_TIME)
-                
-                if(firstGo){
-                    firstGo = false
-                    🚄
-                    
-                    document.removeEventListener("keydown", keyDownHandler)
-                    
-                    console.log("removed listener") 
-                    freezeInterval = setInterval(addListener, 3000)
-                    
-                }
-                
-            }
-
-        }
-        
-    }
 
     function getAvatar(id){
         fetch(`${BASE_URL}${FROG_URL}/${id}`)
@@ -240,8 +423,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 frogTag.innerText = frog.avatar
                 frogTag.style.top = `${BOTTOM_EDGE - 27}px`
                 frogTag.style.left = `${RIGHT_EDGE/2}px`
+              
+                prizeTag.innerText = frog.prize
+                prizeTag.style.top = `${TOP_EDGE - 3}px`;
+                prizeTag.style.left = `${Math.floor(Math.random()* WIDTH ) - 20}px`
 
-
+                document.addEventListener("keydown", keyDownHandler)
             })
         
     }
@@ -256,7 +443,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // on submit, hide the form, show the gameboard
 
     //fetch and Add the avatar
-    let id =1 //this will be changed when we do form
+    let id =5 //this will be changed when we do form
     getAvatar(id)
 
     //get the lanes
